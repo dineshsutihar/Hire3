f94dedb71fad073c4a5532887b898de5e5e8d3fe
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2026-01-15 16:05:49 +0530

    feat(db): add migration for Job details fields

---
+++ backend/prisma/migrations/20260115103438_add_job_details_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "public"."Job" ADD COLUMN     "company_name" TEXT,
+ADD COLUMN     "company_type" TEXT,
+ADD COLUMN     "experience_level" TEXT,
+ADD COLUMN     "industry" TEXT,
+ADD COLUMN     "role" TEXT,
+ADD COLUMN     "salary_range" TEXT;
